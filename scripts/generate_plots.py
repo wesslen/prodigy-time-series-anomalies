18137782606a78cc6b2a98f7673941c8feda4186
Ryan Wesslen <ryan@explosion.ai>
2023-09-19 11:32:15 -0400

    change all_data plot for only by month

--- scripts/generate_plots.py
+++ scripts/generate_plots.py
@@ -3,6 +3,7 @@ import os
 import srsly
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
+import matplotlib.dates as mdates
 import pandas as pd
 
 app = typer.Typer()
@@ -33,22 +34,30 @@ def generate_plots(
             all_data = pd.concat([all_data, df])
 
         # Filter records based on the threshold
-        filtered_records = [record for record in stats_records if record['stdev'] > threshold]
+        filtered_records = [record for record in stats_records if record['stdev'] > threshold or record['stdev'] < 1]
 
         # Convert the 'timestamp' column to a timestamptime type
-        # all_data['timestamp'] = pd.to_timestamptime(all_data['timestamp'])
+        all_data['timestamp'] = pd.to_datetime(all_data['timestamp'])
 
         # Generate and save individual plots for filtered records with formatted x-axis labels
         for i, record in enumerate(filtered_records):
             file_name = record['file']
             csv_file_path = os.path.join(input_files, file_name)
             df = pd.read_csv(csv_file_path)
+            df['timestamp'] = pd.to_datetime(df['timestamp'])
+
+            first_record_date = df['timestamp'].iloc[0]
+            month_year_string = first_record_date.strftime('%Y-%m')
 
             plt.figure(figsize=(10, 6))
             
             # Plot the individual filtered record
             plt.subplot(211)
             plt.plot(df['timestamp'], df['meter_reading'])
+
+            plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
+            plt.gca().xaxis.set_major_locator(mdates.HourLocator(interval=1))  # Set the interval as needed
+
             plt.xlabel('timestamp')
             plt.ylabel('meter_reading')
             plt.title(f'Plot for Day {i}')
@@ -59,11 +68,16 @@ def generate_plots(
 
             # Plot the overall time series
             plt.subplot(212)
-            plt.plot(all_data['timestamp'], all_data['meter_reading'], color='blue')
+            month_data = all_data[all_data['timestamp'].dt.strftime('%Y-%m') == month_year_string]
+            plt.plot(month_data['timestamp'], month_data['meter_reading'], color='blue')
+
+            plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m-%d'))
+            plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=1))  # Set the interval as needed
+
             plt.xlabel('timestamp')
             plt.ylabel('meter_reading')
-            plt.title('Annual Temperature Plot')
-            plt.fill_between(all_data['timestamp'], all_data['meter_reading'].min(), all_data['meter_reading'].max(), where=(all_data['timestamp']>record['start']) & (all_data['timestamp']<=record['end']), alpha=0.2, color='blue', label='Shading')
+            plt.title(f'Temperature Plot for {month_year_string}')
+            plt.fill_between(month_data['timestamp'], month_data['meter_reading'].min(), month_data['meter_reading'].max(), where=(month_data['timestamp']>record['start']) & (month_data['timestamp']<=record['end']), alpha=0.2, color='blue', label='Shading')
             
             ax = plt.gca()
             ax.xaxis.set_major_locator(ticker.MaxNLocator(nbins=12))
